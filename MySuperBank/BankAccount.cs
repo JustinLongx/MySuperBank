6926c5d53e491c1518b9eda95a012c97e0cd075f
justin long <justinxlong93@gmail.com>
2022-01-06 14:52:55 -0600

    Added MakeWithdrawl methods for a hammock and xbox game and decresed overall banking balance.

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -16,7 +16,7 @@ namespace MySuperBank
                 decimal balance = 0;
                 foreach (var item in allTransactions)
                 {
-                    balance = balance = item.Amount;
+                    balance += item.Amount;
                 }
                 return balance;
             
@@ -25,21 +25,36 @@ namespace MySuperBank
         private static int accountNumberSeed = 1234567890;
 
         private List<Transaction> allTransactions = new List<Transaction>();
-        public BankAccount(string name)
+        public BankAccount(string name, decimal initialBalance)
         {
             Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
             Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)// if deposit is equal or less than zero it will throw an exception.
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)// cant withdraw 0
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)// cant withdraw more than our balance.
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
